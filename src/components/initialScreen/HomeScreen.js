dcb5f8b11f804d48c1ce979521d7b1c3421bb2f2
David <46385259+Daviddg91@users.noreply.github.com>
2021-04-15 02:53:03 +0200

    modified:   src/components/initialScreen/HomeScreen.js

--- src/components/initialScreen/HomeScreen.js
+++ src/components/initialScreen/HomeScreen.js
@@ -6,19 +6,18 @@ export const HomeScreen = () => {
             <div className="d-flex justify-content-center">
             <h3>Bienvenido</h3>
             </div>
-            <div className="d-flex">
-                <div className="col-6">
-                <img src={mernLogo} alt="Logo" className="col-12" />
-                </div>
-                <div className="col-6">
-                Esta es una aplicacion web desarollada con el Framework React, el cual realiza funcionalidades CRUD explotando una API 
+            
+                <div className="col-10 m-auto ">
+                <img src={mernLogo} alt="Logo" className="col-12" height="100px" />
+				  Esta es una aplicacion web desarollada con el Framework React, el cual realiza funcionalidades CRUD explotando una API 
                 desarrollada con Express y Mongoose, esta realiza las funcionalidades CRUD conectandose a una base de datos MongoDB en 
                 la nube (Mongo Atlas) y respondiendo a los formularios reactivos con validaciones y control de consistencia de datos.
                 <br></br>
                 Esta aplicación implementa redux y todos sus componentes se comunican con redux para realizar las funcionalidades desarolladas 
                 para el CRUD. Incluye login con JWT y registro de usuarios.
                 </div>
-            </div>
+                
+            
             
 
         </div>
